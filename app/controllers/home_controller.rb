50e73205cc085000b814a2bb59ff79e6be20ce5b
harish <harish@harish-desktop.(none)>
2011-07-11 11:48:09 +1000

    rumble client project added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,60 @@
+require 'open-uri'
+require 'json'
+
+class HomeController < ApplicationController
+
+  def index
+    fetch_rumble_data
+    fetch_events_data
+  end
+
+  def fetch_rumble_data
+    url = 'http://localhost:8080/rumbles/1'
+    content = open(url).read
+    @rumble = JSON.parse(content)
+  end
+
+  def fetch_events_data
+    url = 'http://localhost:8080/rumble_log/1'
+    content = open(url).read
+    json = JSON.parse(content)
+    @events = events(json)
+  end
+
+  def events json
+    events = json["events"] || []
+    messages = []
+    events.each do |event|
+      content = event["event_at"][11..18]
+
+      team = event["team"]
+      fan = event["fan"]
+      content += ' '
+
+      content += team if team
+      content += fan if fan
+
+      event_name = event["event"]
+      content += ', ' unless event_name.blank?
+      content += event_name if event_name
+      msge = event["message"]
+      content += ', ' unless msge.blank?
+      content += msge if msge
+
+      diff = event[":type"] == "game_event" ? "+" : "-"
+      health_value_adjust = event["health_value_adjust"]
+      if !health_value_adjust.blank? && health_value_adjust.to_i > 0
+        content += ", Health #{diff}#{health_value_adjust}%"
+      end
+
+      attack_value_adjust = event["attack_value_adjust"]
+      if !attack_value_adjust.blank?
+        content += ", Attack #{diff}#{attack_value_adjust}"
+      end
+
+      messages << content
+    end
+    messages
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RumbleClient::Application.config.secret_token = '9d413c9a237c33eda8b2c3281035461afd07fc6be3669df3a90d0cbe0f0561b78038a2bc7c42b1b47c1be2c4ff4dd2f5b9c785d53025a468f18ec0d3bddce4f6'
